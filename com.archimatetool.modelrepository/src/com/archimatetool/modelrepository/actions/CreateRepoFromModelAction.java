22ba6c2d0082e1b8b01f0be473bc34af01d2b32b
Phillipus <p.beauvoir@dadabeatnik.com>
2020-08-30 12:00:15 +0100

    CreateRepoFromModelAction stores remote and user info in all cases and reports errors
    * Store remote and user details in all cases
    * Report any push errrors
    * On a push error remove the remote

--- com.archimatetool.modelrepository/src/com/archimatetool/modelrepository/actions/CreateRepoFromModelAction.java
+++ com.archimatetool.modelrepository/src/com/archimatetool/modelrepository/actions/CreateRepoFromModelAction.java
@@ -10,10 +10,14 @@ import java.io.File;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.window.Window;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.RemoteRefUpdate;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 
 import com.archimatetool.editor.model.IEditorModelManager;
+import com.archimatetool.editor.utils.StringUtils;
 import com.archimatetool.model.IArchimateModel;
 import com.archimatetool.modelrepository.IModelRepositoryImages;
 import com.archimatetool.modelrepository.authentication.ProxyAuthenticator;
@@ -52,6 +56,9 @@ public class CreateRepoFromModelAction extends AbstractModelAction {
         }
         
         final File folder = dialog.getFolder();
+        final String repoURL = dialog.getURL();
+        final boolean storeCredentials = dialog.doStoreCredentials();
+        final UsernamePassword npw = dialog.getUsernamePassword();
         
         // Ensure folder exists
         folder.mkdirs();
@@ -62,6 +69,11 @@ public class CreateRepoFromModelAction extends AbstractModelAction {
             // Init
             getRepository().init();
             
+            // Add the remote if it's set
+            if(StringUtils.isSet(repoURL)) {
+                getRepository().addRemote(repoURL);
+            }
+            
             // Set new file location
             fModel.setFile(getRepository().getModelFile());
             
@@ -71,50 +83,83 @@ public class CreateRepoFromModelAction extends AbstractModelAction {
             // Commit changes
             getRepository().commitChanges(Messages.CreateRepoFromModelAction_1, false);
             
-            // If we want to publish now...
-            if(response == NewRepoDialog.ADD_AND_PUBLISH_ID) {
-                final String repoURL = dialog.getURL();
-                final boolean storeCredentials = dialog.doStoreCredentials();
-                final UsernamePassword npw = dialog.getUsernamePassword();
-                
-                // Add the remote
-                getRepository().addRemote(repoURL);
-                
-                // Proxy check
-                ProxyAuthenticator.update(repoURL);
-                
-                // Push
-                Exception[] exception = new Exception[1];
-                
-                PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress() {
-                    @Override
-                    public void run(IProgressMonitor pm) {
-                        try {
-                            pm.beginTask(Messages.CreateRepoFromModelAction_2, IProgressMonitor.UNKNOWN);
-                            getRepository().pushToRemote(npw, new ProgressMonitorWrapper(pm));
-                        }
-                        catch(Exception ex) {
-                            exception[0] = ex;
-                        }
-                    }
-                });
+            // Add to the Tree Model
+            RepositoryTreeModel.getInstance().addNewRepositoryRef(getRepository());
 
-                if(exception[0] != null) {
-                    throw exception[0];
-                }
-                
-                // Store repo credentials if HTTP and option is set
-                if(RepoUtils.isHTTP(repoURL) && storeCredentials) {
-                    SimpleCredentialsStorage scs = new SimpleCredentialsStorage(new File(getRepository().getLocalGitFolder(), REPO_CREDENTIALS_FILE));
-                    scs.store(npw);
-                }
+            // If we want to publish now then push...
+            if(response == NewRepoDialog.ADD_AND_PUBLISH_ID) {
+                push(repoURL, npw);
             }
             
-            // Add to the Tree Model last so that the correct status is shown
-            RepositoryTreeModel.getInstance().addNewRepositoryRef(getRepository());
+            // Store repo credentials if HTTP and option is set
+            if(RepoUtils.isHTTP(repoURL) && storeCredentials) {
+                SimpleCredentialsStorage scs = new SimpleCredentialsStorage(new File(getRepository().getLocalGitFolder(), REPO_CREDENTIALS_FILE));
+                scs.store(npw);
+            }
         }
         catch(Exception ex) {
             displayErrorDialog(Messages.CreateRepoFromModelAction_0, ex);
         }
+    }
+    
+    /**
+     * Push to remote
+     */
+    private void push(final String repoURL, final UsernamePassword npw) throws Exception {
+        // Proxy check
+        ProxyAuthenticator.update(repoURL);
+        
+        // Store exception
+        Exception[] exception = new Exception[1];
+        
+        // Push
+        PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress() {
+            @Override
+            public void run(IProgressMonitor pm) {
+                try {
+                    pm.beginTask(Messages.CreateRepoFromModelAction_2, IProgressMonitor.UNKNOWN);
+                    Iterable<PushResult> pushResult = getRepository().pushToRemote(npw, new ProgressMonitorWrapper(pm));
+                    
+                    // Get any errors in Push Results
+                    String errorMessage = getPushResultErrorMessage(pushResult);
+                    if(errorMessage.length() > 0) {
+                        throw new GitAPIException(errorMessage) {};
+                    }
+                }
+                catch(Exception ex) {
+                    exception[0] = ex;
+                }
+            }
+        });
+
+        if(exception[0] != null) {
+            // In case of an exception remove the remote
+            getRepository().removeRemote();
+            throw exception[0];
+        }
+    }
+    
+    /**
+     * Get any errors in Push Results
+     */
+    private String getPushResultErrorMessage(Iterable<PushResult> pushResult) {
+        StringBuilder sb = new StringBuilder();
+        
+        pushResult.forEach(result -> {
+            result.getRemoteUpdates().stream()
+                    .filter(update -> update.getStatus() != RemoteRefUpdate.Status.OK)
+                    .filter(update -> update.getStatus() != RemoteRefUpdate.Status.UP_TO_DATE)
+                    .forEach(update -> {
+                        if(StringUtils.isSet(result.getMessages())) {
+                            sb.append(result.getMessages() + "\n"); //$NON-NLS-1$
+                        }
+                        else {
+                            sb.append(update.getStatus().toString() + "\n"); //$NON-NLS-1$
+                        }
+                    });
+            
+        });
+        
+        return sb.toString();
     }
 }
