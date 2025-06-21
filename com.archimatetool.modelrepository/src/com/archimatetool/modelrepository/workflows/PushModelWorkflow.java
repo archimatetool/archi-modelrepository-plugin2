4ae5951a9d18e555b98a1c17fc056ce6202a0d4f
Phillipus <p.beauvoir@dadabeatnik.com>
2025-06-21 18:18:19 +0100

    Check if there are remote commits and do dry run fetch before Publish

--- com.archimatetool.modelrepository/src/com/archimatetool/modelrepository/workflows/PushModelWorkflow.java
+++ com.archimatetool.modelrepository/src/com/archimatetool/modelrepository/workflows/PushModelWorkflow.java
@@ -14,13 +14,21 @@ import org.eclipse.equinox.security.storage.StorageException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.FetchResult;
 import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.swt.SWT;
 import org.eclipse.ui.IWorkbenchWindow;
 
 import com.archimatetool.editor.ui.components.IRunnable;
+import com.archimatetool.modelrepository.authentication.CredentialsAuthenticator;
 import com.archimatetool.modelrepository.authentication.UsernamePassword;
+import com.archimatetool.modelrepository.dialogs.Dialogs;
+import com.archimatetool.modelrepository.repository.BranchInfo;
+import com.archimatetool.modelrepository.repository.BranchInfo.Option;
+import com.archimatetool.modelrepository.repository.GitUtils;
 import com.archimatetool.modelrepository.repository.IArchiRepository;
 import com.archimatetool.modelrepository.repository.IRepositoryListener;
+import com.archimatetool.modelrepository.repository.RepoConstants;
 import com.archimatetool.modelrepository.repository.RepoUtils;
 
 /**
@@ -38,9 +46,6 @@ public class PushModelWorkflow extends AbstractPushResultWorkflow {
 
     @Override
     public void run() {
-        // TODO: Refresh (fetch) and resolve any merge conflicts first
-        
-        // TODO: This (and model dirty/needs committing) should be done in Refresh (fetch)
         if(!checkRemoteSet()) {
             return;
         }
@@ -60,36 +65,101 @@ public class PushModelWorkflow extends AbstractPushResultWorkflow {
             return;
         }
         
-        PushResult pushResult = null;
+        ProgressMonitorDialog dialog = new ProgressMonitorDialog(workbenchWindow.getShell());
+        
+        // Check if remote is ahead or Fetch needed first
+        try(GitUtils utils = GitUtils.open(archiRepository.getGitFolder())) {
+            // If there are ahead commits...
+            BranchInfo branchInfo = BranchInfo.currentLocalBranchInfo(archiRepository.getGitFolder(), Option.COMMIT_STATUS);
+            if(branchInfo.hasRemoteCommits()) {
+                int response = Dialogs.openYesNoCancelDialog(workbenchWindow.getShell(), Messages.PushModelWorkflow_0,
+                        "There are new updates on the remote repository, do you want to Refresh first?");
+                
+                if(response == SWT.YES) {
+                    RefreshModelWorkflow workflow = new RefreshModelWorkflow(workbenchWindow, archiRepository);
+                    workflow.run();
+                    return;
+                }
+                
+                if(response == SWT.CANCEL) {
+                    return;
+                }
+            }
+            
+            FetchResult fetchResult = fetchDryRun(npw, dialog);
+            if(!fetchResult.getTrackingRefUpdates().isEmpty()) {
+                int response = Dialogs.openYesNoCancelDialog(workbenchWindow.getShell(), Messages.PushModelWorkflow_0,
+                        "There are new updates on the remote repository, do you want to Refresh first?");
+                
+                if(response == SWT.YES) {
+                    RefreshModelWorkflow workflow = new RefreshModelWorkflow(workbenchWindow, archiRepository);
+                    workflow.run();
+                    return;
+                }
+                
+                if(response == SWT.CANCEL) {
+                    return;
+                }
+            }
+        }
+        catch(Exception ex) {
+            logger.log(Level.SEVERE, "Fetch", ex); //$NON-NLS-1$
+            displayErrorDialog(Messages.PushModelWorkflow_0, ex);
+            return;
+        }
         
         try {
-            pushResult = push(npw);
+            PushResult pushResult = push(npw, dialog);
+            
+            // Logging
+            logPushResult(pushResult, logger);
+            
+            // Show result
+            showPushResult(pushResult);
         }
         catch(Exception ex) {
             logger.log(Level.SEVERE, "Push", ex); //$NON-NLS-1$
             displayErrorDialog(Messages.PushModelWorkflow_0, ex);
         }
-        finally {
-            // Notify listeners
-            notifyChangeListeners(IRepositoryListener.HISTORY_CHANGED); // This will also update Branches View
-        }
         
-        // Logging
-        logPushResult(pushResult, logger);
+        // Notify listeners
+        notifyChangeListeners(IRepositoryListener.HISTORY_CHANGED); // This will also update Branches View
+    }
+    
+    /**
+     * Do a Fetch update
+     */
+    private FetchResult fetchDryRun(UsernamePassword npw, ProgressMonitorDialog dialog) throws Exception {
+        logger.info("Fetching with dry run from " + archiRepository.getRemoteURL()); //$NON-NLS-1$
+
+        FetchResult[] fetchResult = new FetchResult[1];
         
-        // Show result
-        showPushResult(pushResult);
+        IRunnable.run(dialog, monitor -> {
+            monitor.beginTask("Checking remote for updates...", IProgressMonitor.UNKNOWN);
+            
+            try(GitUtils utils = GitUtils.open(archiRepository.getGitFolder())) {
+                String currentBranch = utils.getRepository().getBranch();
+                String refSpec = RepoConstants.R_HEADS + currentBranch + ":" + 
+                                            RepoConstants.R_REMOTES_ORIGIN + currentBranch ;
+                
+                fetchResult[0] = utils.fetch()
+                                      .setTransportConfigCallback(CredentialsAuthenticator.getTransportConfigCallback(npw))
+                                      .setDryRun(true)
+                                      .setRefSpecs(refSpec)
+                                      .call();
+            }
+        }, true);
+        
+        return fetchResult[0];
     }
     
     /**
      * Push to Remote
      */
-    private PushResult push(UsernamePassword npw) throws Exception {
+    private PushResult push(UsernamePassword npw, ProgressMonitorDialog dialog) throws Exception {
         logger.info("Pushing to " + archiRepository.getRemoteURL()); //$NON-NLS-1$
 
         PushResult[] pushResult = new PushResult[1];
-        
-        ProgressMonitorDialog dialog = new ProgressMonitorDialog(workbenchWindow.getShell());
         
         IRunnable.run(dialog, monitor -> {
             monitor.beginTask(Messages.PushModelWorkflow_1, IProgressMonitor.UNKNOWN);
